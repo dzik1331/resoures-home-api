2f415bdcd03d5d76d6f942a4daab1028720c7f9c
Krzysztof Nieradzik <krzynie1331@gmail.com>
2018-01-03 18:47:55 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var fs = require("fs");
 var pgp = require('pg-promise')(/*options*/)
-var db = pgp('postgres://postgres:Qwert!2345@localhost:5432/words')
+var db = pgp('postgres://postgres:Qwert!2345@localhost:5432/books-list')
 // var db = pgp('postgres://p1069_words:Qwert!2345@pgsql15.mydevil.net:5432/p1069_words')
 
 
-
-/* GET home page. */
-router.post('/search', function(req, res, next) {
-  var params = req.body;
-  var sql = "SELECT DISTINCT words FROM words WHERE len=$1 and words ~ $2";
-  db.query(sql,[params.len, getRegex(params.letters)])
-  .then(function (data) {
-    console.log('DATA:', data)
-    var counts = getCounts(params.letters.split(''))
-    res.json(checkMultiLetters(data, counts));
-  })
-  .catch(function (error) {
-    console.log('ERROR:', error)
-  })
+router.get('/list', function (req, res, next) {
+    var sql = "SELECT b.title, b.author, b.img, t.name AS type, b.date FROM book AS b LEFT JOIN types AS t ON t.id = b.type;";
+    db.query(sql)
+        .then(function (data) {
+            console.log('DATA:', data)
+            res.json(data);
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
 });
 
-function getRegex(letters) {
-	var _letters = letters.split('');
-	var count = getCounts(_letters);
-	var uniqeLetters = _.uniq(_letters)
-	var regexLetter = '';
-	uniqeLetters.forEach((letter) => {
-		if (count[letter] == 1) {
-			regexLetter += '(?!.*'+ letter +'.*'+ letter +')'
-		}
-	})
-	return '^'+ regexLetter +'['+ letters +']*$';
-}
+router.get('/types', function (req, res, next) {
+    var sql = "SELECT id, name from types";
+    db.query(sql)
+        .then(function (data) {
+            console.log('DATA:', data)
+            res.json(data);
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+});
 
-function checkMultiLetters(words, counts) {
-	var _copyWords = _.cloneDeep(words)
-	_copyWords.forEach((word, index) => {
-		var wordCounts = _.countBy(word.words.split(''))
-		console.log(word)
-		Object.keys(wordCounts).some((key) => {
-			console.log(wordCounts[key] + ' ' + counts[key]);
-			if (wordCounts[key] > counts[key]) {
-				console.log('usuwam')
-				_.remove(words, word)
-				return true;
-			}
-		});
-	});
-	return words;
-}
+router.post('/add', function (req, res, next) {
+    console.log(req.body)
+    var data = req.body;
+    var sql = "INSERT INTO public.book(title, author, date, type, img) VALUES ($1, $2, $3, $4, $5)";
+    db.query(sql,[data.title, data.author, new Date(), data.type, data.image.filename])
+        .then(function (data) {
+            res.json('OK');
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
 
-function getCounts(letters) {
-	return _.countBy(letters);
-}
+    fs.writeFile("public/images/" + req.body.image.filename, req.body.image.value, 'base64', function (err) {
+        console.log(err);
+    });
+});
+
+router.post('/upload', function (req, res, next) {
+    // console.log(req)
+    console.log(req.files)
+    console.log(res.files)
+    res.json({test: 123});
+});
 
 module.exports = router;
